e47f43c4dddae4061eaf498570d42f7119541eb3
Jason Lynch <jason@calindora.com>
2020-11-03 21:00:27 -0800

    Open the application when the notification is tapped.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- app/src/main/java/com/calindora/follow/FollowService.kt
+++ app/src/main/java/com/calindora/follow/FollowService.kt
@@ -1,9 +1,6 @@
 package com.calindora.follow
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
 import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
@@ -105,10 +102,17 @@ class FollowService : Service() {
      */
 
     private fun createNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val notification = Notification.Builder(this, "com.calindora.follow.default")
             .setSmallIcon(R.drawable.ic_stat_notification)
             .setContentTitle(getText(R.string.notification_title))
             .setContentText(getText(R.string.notification_text))
+            .setContentIntent(pendingIntent)
             .build()
 
         startForeground(1, notification)
