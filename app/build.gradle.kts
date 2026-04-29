43acc160ffb2a697630e2498b46b3dd37722e764
Jason Lynch <jason@aexoden.com>
2026-04-29 14:17:15 -0700

    build: change dependency to coroutines.android
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
   implementation(libs.androidx.room.ktx)
   implementation(libs.androidx.room.runtime)
   implementation(libs.androidx.work.runtime.ktx)
-  implementation(libs.kotlinx.coroutines.core)
+  implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.serialization.json)
   implementation(libs.material)
   implementation(libs.okhttp)
