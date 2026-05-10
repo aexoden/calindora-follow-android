49104257d8eb6dfbdecc0a349808647e73acaf47
Jason Lynch <jason@aexoden.com>
2026-05-10 13:28:37 -0700

    refactor: use stdlib toHexString for HMAC encoding
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- app/src/main/java/com/calindora/follow/Signing.kt
+++ app/src/main/java/com/calindora/follow/Signing.kt
@@ -8,5 +8,5 @@ internal fun hmacSha256Hex(input: String, secret: String): String {
   val mac = Mac.getInstance("HmacSHA256")
   mac.init(SecretKeySpec(secret.toByteArray(Charsets.UTF_8), mac.algorithm))
   val digest = mac.doFinal(input.toByteArray(Charsets.UTF_8))
-  return digest.joinToString("") { "%02x".format(it) }
+  return digest.toHexString()
 }
